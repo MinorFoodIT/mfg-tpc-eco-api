ce8be56ccbc5ad72e428c47016ba0272a78bac21
akarat <akrs33@gmail.com>
2021-02-15 18:04:42 +0700

    Dev: added two endpoint

--- database/dbClient.js
+++ database/dbClient.js
@@ -21,6 +21,40 @@ var helper = require('./../common/helper');
 
 // }
 
+const queryFunc =  (err,client,query,params) => {
+    return new Promise ((resolve, reject) => {
+        if(err){
+            logger.info('[POOL] connection error : ')
+            console.log(err)
+            try{
+                client.release();
+                resolve(null)
+            }catch(e){
+                reject(null)
+            }
+        }else{
+            try{
+                client.query(query ,params, function (error, results, fields){
+                    client.release();
+                    if(error){
+                        logger.info('[POOL] query error : ')
+                        console.log(error)
+                        resolve(null)
+                    }else{
+                        resolve(results)
+                    }
+                })
+            }catch(tryErr){
+                logger.info('[POOL] try catch query error : ')
+                console.log(tryErr)
+                reject(null)
+            }
+        }
+
+    })
+    
+}
+
 const getLottoByCode = async(lotto) => {
 
     return new Promise ((resolve, reject) => {
@@ -28,27 +62,30 @@ const getLottoByCode = async(lotto) => {
             let params = [lotto.code,lotto.typeCode] 
             console.log(params)
             pool((err, client) => {
-                if(err){
-                    console.log(err)
-                    try{
-                        client.release();
-                        reject(null)
-                    }catch(e){
-                        reject(null)
-                    }
-                }else{
-                    client.query('SELECT code,storeCode from lotto where code=? and typeCode=? ',params, function (error, results, fields){
-                        if(error){
-                            console.log(error)
-                            resolve({})
-                            client.release();
-                        }else{
-                            resolve(results)
-                            client.release();
-                        }
+                let sql_query = 'SELECT code,storeCode from lotto where code=? and typeCode=? '
+                let results = await queryFunc(err,client,sql_query,params)
+                resolve(results)
+                // if(err){
+                //     console.log(err)
+                //     try{
+                //         client.release();
+                //         reject(null)
+                //     }catch(e){
+                //         reject(null)
+                //     }
+                // }else{
+                //     client.query('SELECT code,storeCode from lotto where code=? and typeCode=? ',params, function (error, results, fields){
+                //         if(error){
+                //             console.log(error)
+                //             resolve({})
+                //             client.release();
+                //         }else{
+                //             resolve(results)
+                //             client.release();
+                //         }
 
-                    })
-                }
+                //     })
+                // }
 
             })
          }
