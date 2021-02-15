b12edbd8c5581da5c04787b849c64f11be17408d
akarat <akrs33@gmail.com>
2021-02-15 14:40:31 +0700

    Dev: added two endpoint

---
+++ routes/app/app-route.js
@@ -0,0 +1,83 @@
+var express = require("express")
+var logger = require("./../../common/logging/winston")(__filename)
+var router = express.Router()
+const fs = require('fs')
+
+const Lotto = require('../../database/model/foodmarketing').Lotto;
+const dbservice = require('./../../database/services/dbservice');
+const helper = require('./../../common/helper')
+const asyncMiddleware = require('../../utils/asyncMiddleWare')
+const APIResponse = require("./../../common/APIResponse")
+const APIError = require("./../../common/APIError")
+
+const responseError = (res, message ,status, isPublic) => {
+    res.status(status).json(new APIError(message, status, isPublic).jsonReturn());
+}
+
+router.post("/v1/lotto", asyncMiddleware(async (req, res, next) => {
+    logger.info("[LOTTO] POST /v1/lotto")
+    let client_id = req.headers["client_id"]
+    if(!helper.isNullEmptry(client_id) && client_id === 'minorfoodit'){
+        logger.info("[LOTTO] code "+ req.body.code )
+        logger.info("[LOTTO] store "+ req.body.storeCode )
+        let lotto = new Lotto() 
+        lotto.brand = req.body.brand
+        lotto.promotion = req.body.promotion
+        lotto.typeCode = new String(req.body.typeCode).toUpperCase()
+        lotto.code = req.body.code
+        lotto.telephone = req.body.telephone
+        lotto.firstName = req.body.firstName
+        lotto.lastName = req.body.lastName
+        lotto.storeCode = req.body.storeCode
+        lotto.posDate = req.body.posDate
+        lotto.posData = JSON.stringify(req.body)
+        lotto.requestID = req.body.requestID
+        lotto.posFlag = 'posSaved';
+        let newLotto = await dbservice.saveLotto(lotto)
+        if(newLotto){
+            res.json(
+                new APIResponse(
+                    "Success", 
+                    200, 
+                    newLotto, 
+                    true
+            ).jsonReturn())
+        }else{
+            //Did not success
+            responseError(res, "Code is error", 500, true)
+        } 
+    }else{
+        //Authen is invalid
+        responseError(res, "Client_id is invalid", 403, true)
+    }
+
+}))
+
+router.get("/v1/lotto/:typecode/:code", asyncMiddleware(async (req, res, next) => {
+    let client_id = req.headers["client_id"]
+    if(!helper.isNullEmptry(client_id) && client_id === 'minorfoodit'){
+        let typecode = req.params.typecode;
+        let code = req.params.code;
+        let lotto = new Lotto() 
+        lotto.code = code
+        lotto.typeCode = typecode
+        let lotto = await dbservice.getLottoByCode(lotto)
+        if (lotto) {
+            res.json(
+                new APIResponse(
+                    "Success", 
+                    200, 
+                    lotto, 
+                    true 
+            ).jsonReturn())
+        } else {
+            error(res, "Code did not found", 404, true)
+        }
+    }else{
+        //Authen is invalid
+        responseError(res, "Client_id is invalid", 403, true)
+    }
+  
+}))
+
+module.exports = router;
